8bad23f1e06c75d3bcd829db23155871fab10bbf
projecti18n <github.com@androssow.com>
2023-04-15 08:48:56 +0200

    test

--- module5/js/script.js
+++ module5/js/script.js
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML(categories),
+  buildAndShowHomeHTML,
   //function (categories){buildAndShowHomeHTML(categories)}, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
